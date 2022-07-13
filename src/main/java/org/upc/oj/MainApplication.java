94b79f2fa97bfe12b24ee060e5bf2f7b3fcb2dec
yqs <281876918@qq.com>
2022-07-13 21:35:39 +0800

    first commit

---
+++ src/main/java/org/upc/oj/MainApplication.java
@@ -0,0 +1,13 @@
+package org.upc.oj;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/upc/MainApplicationTest.java
@@ -0,0 +1,20 @@
+package org.upc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
