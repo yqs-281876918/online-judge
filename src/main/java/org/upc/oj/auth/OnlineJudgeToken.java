1d8e0806c2e323e9af9be483cc10421246edcab6
yqs <281876918@qq.com>
2022-07-14 14:37:14 +0800

    添加token的序列化与反序列化

--- src/main/java/org/upc/oj/auth/OnlineJudgeToken.java
+++ src/main/java/org/upc/oj/auth/OnlineJudgeToken.java
@@ -1,2 +1,19 @@
-package org.upc.oj.auth;public class OnlineJudgeToken {
+package org.upc.oj.auth;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+public class OnlineJudgeToken {
+    private String userName;//用户名
+    private Long createAt;//创建时间
+    private Long expireAt;//到期时间
+    private String identity;//身份
+    private Object extra;//额外信息
+    public OnlineJudgeToken(){
+        createAt=new Date().getTime();
+    }
 }
