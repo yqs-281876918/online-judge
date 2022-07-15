48ae9305644359361da33959127e298f54060924
yqs <281876918@qq.com>
2022-07-15 17:59:44 +0800

    细化judger.exe评测结果信息

--- src/main/java/org/upc/oj/bank/service/TagService.java
+++ src/main/java/org/upc/oj/bank/service/TagService.java
@@ -2,11 +2,20 @@ package org.upc.oj.bank.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.upc.oj.MainApplication;
 import org.upc.oj.bank.dao.TagMapper;
+
+import java.util.Scanner;
 
 @Service
 public class TagService {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        System.out.println(Math.max(a,b));
+    }
     @Autowired
     private TagMapper tagMapper;
 
