56511daf75532db8d7ec3cf93589945e771668dc
Haunser <3192782915@qq.com>
2022-07-15 20:20:48 +0800

    测试用例 结构体

---
+++ src/main/java/org/upc/oj/judger/po/TestIOFile.java
@@ -0,0 +1,22 @@
+package org.upc.oj.judger.po;
+
+public class TestIOFile {
+    private String Innput;
+    private String Output;
+
+    public String getInnput() {
+        return Innput;
+    }
+
+    public void setInnput(String innput) {
+        Innput = innput;
+    }
+
+    public String getOutput() {
+        return Output;
+    }
+
+    public void setOutput(String output) {
+        Output = output;
+    }
+}
