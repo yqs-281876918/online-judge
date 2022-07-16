499cfec2cd1d0fd14b781c64a47057b99c772036
Haunser <3192782915@qq.com>
2022-07-16 16:44:51 +0800

    增加 id

--- src/main/java/org/upc/oj/judger/po/TestIOFile.java
+++ src/main/java/org/upc/oj/judger/po/TestIOFile.java
@@ -3,6 +3,15 @@ package org.upc.oj.judger.po;
 public class TestIOFile {
     private String Innput;
     private String Output;
+    private  String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getInnput() {
         return Innput;
