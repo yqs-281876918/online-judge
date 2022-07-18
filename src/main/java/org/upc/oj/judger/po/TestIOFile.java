79073bcc8abf971fdde4f05fd16d6eb740047597
Haunser <3192782915@qq.com>
2022-07-18 22:09:43 +0800

    ==

---
+++ src/main/java/org/upc/oj/judger/po/TestIOFile.java
@@ -0,0 +1,31 @@
+package org.upc.oj.judger.po;
+
+public class TestIOFile {
+    private String Innput;
+    private String Output;
+    private  String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getInnput() {
+        return Innput;
+    }
+
+    public void setInnput(String innput) {
+        Innput = innput;
+    }
+
+    public String getOutput() {
+        return Output;
+    }
+
+    public void setOutput(String output) {
+        Output = output;
+    }
+}
