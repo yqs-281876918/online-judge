2f3a6a91d0bcb3d0d2724859a8b45fca6c4392d9
yqs <281876918@qq.com>
2022-07-23 20:47:32 +0800

    cros

--- src/main/java/org/upc/oj/auth/config/WebConfig.java
+++ src/main/java/org/upc/oj/auth/config/WebConfig.java
@@ -1,10 +1,21 @@
 package org.upc.oj.auth.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.upc.oj.auth.interceptor.GateWayFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST","DELETE","PATCH","PUT")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .maxAge(Integer.MAX_VALUE);
+    }
 }
